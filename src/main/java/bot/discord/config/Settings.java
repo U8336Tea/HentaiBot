ab75a7afa7cb31788cd87b90dbcc6731120d7961
U8336Tea <20009895+U8336Tea@users.noreply.github.com>
2017-10-18 21:43:45 -0600

    Added Settings class

---
+++ src/main/java/bot/discord/config/Settings.java
@@ -0,0 +1,52 @@
+package bot.discord.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name = "settings")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Settings {
+    //region XML Variables
+    @XmlElement
+    private String token;
+
+    @XmlElement(name = "owner-id")
+    private String ownerId;
+
+    @XmlAttribute
+    private String prefix = "!!";
+
+    @XmlAttribute(name = "alt-prefix")
+    private String altPrefix;
+
+    @XmlElement(name = "postgres-url")
+    private String databaseUrl;
+    //endregion
+
+    @NotNull
+    public String getToken() {
+        return token;
+    }
+
+    @NotNull
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    @NotNull
+    public String getPrefix() {
+        return prefix;
+    }
+
+    @Nullable
+    public String getAltPrefix() {
+        return altPrefix;
+    }
+
+    @NotNull
+    public String getDatabaseUrl() {
+        return databaseUrl;
+    }
+}
