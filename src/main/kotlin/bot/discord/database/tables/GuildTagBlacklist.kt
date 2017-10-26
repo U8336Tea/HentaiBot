42f33a3c2d062555634ca5e8fa05c463aa02c04f
U8336Tea <20009895+U8336Tea@users.noreply.github.com>
2017-10-26 16:06:46 -0600

    Added default for GuildTagTable requirements

--- src/main/kotlin/bot/discord/database/tables/GuildTagBlacklist.kt
+++ src/main/kotlin/bot/discord/database/tables/GuildTagBlacklist.kt
@@ -1,7 +1,3 @@
 package bot.discord.database.tables
 
-internal object GuildTagBlacklist : GuildTagTable("GUILD_TO_BLACKLISTED_TAG") {
-	override val id = integer("id").autoIncrement().primaryKey()
-	override val guild = integer("guild_id") references Guild.id
-	override val tag = integer("tag_id") references Tags.id
-}
+internal object GuildTagBlacklist : GuildTagTable("GUILD_TO_BLACKLISTED_TAG")
