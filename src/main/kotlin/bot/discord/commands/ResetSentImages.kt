68ae4aa4a73de97ee9ca06201ddb6b3ff3b8e94c
U8336Tea <20009895+U8336Tea@users.noreply.github.com>
2017-10-26 17:27:50 -0600

    Added name and help for ResetSentImages

--- src/main/kotlin/bot/discord/commands/ResetSentImages.kt
+++ src/main/kotlin/bot/discord/commands/ResetSentImages.kt
@@ -5,6 +5,11 @@ import com.jagrosh.jdautilities.commandclient.Command
 import com.jagrosh.jdautilities.commandclient.CommandEvent
 
 object ResetSentImages : Command() {
+	init {
+		this.name = "ResetSentImages"
+		this.help = "Resets the list of sent images."
+	}
+
 	override fun execute(event: CommandEvent) {
 		GuildTable.removeSentImages(event.guild)
 		event.replySuccess("Reset sent images!")
